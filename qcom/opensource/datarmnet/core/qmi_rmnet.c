c8afcba9626399438bc9dc4242266fba8bb0abf5
Sharath Chandra Vurukala <quic_sharathv@quicinc.com>
2024-08-28 22:56:58 +0530

    dfc: get notifications for suspend
    Getting notified about suspend and send ps notifications when going into
    suspend.
    
    Change-Id: I31233e5df4633cf848691d76fa15e0cd5416a773
    Signed-off-by: Raul Martinez <quic_mraul@quicinc.com>
    Signed-off-by: Sharath Chandra Vurukala <quic_sharathv@quicinc.com>

--- qcom/opensource/datarmnet/core/qmi_rmnet.c
+++ qcom/opensource/datarmnet/core/qmi_rmnet.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023, 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -691,6 +691,7 @@ __qmi_rmnet_delete_client(void *port, struct qmi_info *qmi, int idx)
 	}
 
 	if (!qmi_rmnet_has_client(qmi) && !qmi_rmnet_has_pending(qmi)) {
+		unregister_pm_notifier(&(((struct rmnet_port *)port)->dfc_pm_notifier));
 		rmnet_reset_qmi_pt(port);
 		kfree(qmi);
 		return 0;
