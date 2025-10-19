a21f1eb7aeb2f5194282bc8ec228ff3a60cee13f
hiroshi <hiroshi@tanuki.page>
2025-10-19 19:22:01 -0400

    noth: touchscreen: Remove KEY_GESTURE_FOD keycode
    We don't use this key event for fingerprint functionality. Occasionally
    this virtual key can get stuck held down after unlocking, which causes
    conflicts with hardware button combinations.
    
    Change-Id: I0fdc736bcba701f78e0253fc3170b33cc9d575c2

--- qcom/opensource/touch-drivers/focaltech/focaltech_gesture.c
+++ qcom/opensource/touch-drivers/focaltech/focaltech_gesture.c
@@ -548,7 +548,6 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_V);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_Z);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_C);
-    input_set_capability(input_dev, EV_KEY, KEY_GESTURE_FOD);
 
     __set_bit(KEY_GESTURE_RIGHT, input_dev->keybit);
     __set_bit(KEY_GESTURE_LEFT, input_dev->keybit);
@@ -564,7 +563,6 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     __set_bit(KEY_GESTURE_V, input_dev->keybit);
     __set_bit(KEY_GESTURE_C, input_dev->keybit);
     __set_bit(KEY_GESTURE_Z, input_dev->keybit);
-    __set_bit(KEY_GESTURE_FOD, input_dev->keybit);
 
 
     fts_create_gesture_sysfs(ts_data->dev);
