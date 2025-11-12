ee6eab310225e5d71f0373f2aaa0448739a3ba0a
hiroshi <hiroshi@tanuki.page>
2025-11-12 00:20:46 -0500

    Revert "Merge commit 'b10d230bc0aba01b2ec8607b10c856542c28da41' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 into lineage-23.0"
    This reverts commit 373991969d08842972394c5562ac59fedcfa5230, reversing
    changes made to 40392121088b00988b754f4ff5616e0b9d1bcbc5.
    Reason: Asteroids_B4.0-251027-1838 -> LA.VENDOR.14.3.0.r1-21900-lanai.QSSI16.0

--- qcom/opensource/wlan/qcacld-3.0/core/mac/src/pe/lim/lim_utils.h
+++ qcom/opensource/wlan/qcacld-3.0/core/mac/src/pe/lim/lim_utils.h
@@ -1054,6 +1054,7 @@ QDF_STATUS lim_send_ies_per_band(struct mac_context *mac_ctx,
 
 /**
  * lim_update_connect_rsn_ie() - Update the connection RSN IE
+ * @mac_ctx: MAC context
  * @session: PE session
  * @rsn_ie_buf: RSN IE buffer
  * @pmksa: PMKSA entry for the connecting AP
@@ -1061,7 +1062,8 @@ QDF_STATUS lim_send_ies_per_band(struct mac_context *mac_ctx,
  * Return: None
  */
 void
-lim_update_connect_rsn_ie(struct pe_session *session, uint8_t *rsn_ie_buf,
+lim_update_connect_rsn_ie(struct mac_context *mac_ctx,
+			  struct pe_session *session, uint8_t *rsn_ie_buf,
 			  struct wlan_crypto_pmksa *pmksa);
 
 /**
@@ -1543,7 +1545,7 @@ void lim_update_session_he_capable_chan_switch(struct mac_context *mac,
  * Return: None
  */
 void lim_set_he_caps(struct mac_context *mac, uint8_t *ie_start,
-		     uint32_t num_bytes, uint8_t band, uint8_t vdev_id);
+		     uint32_t num_bytes, uint8_t band);
 
 /**
  * lim_send_he_caps_ie() - gets HE capability and send to firmware via wma
@@ -1778,9 +1780,9 @@ void lim_update_session_he_capable_chan_switch(struct mac_context *mac,
 {
 }
 
-static inline void lim_set_he_caps(struct mac_context *mac, uint8_t *ie_start,
-				   uint32_t num_bytes, uint8_t band,
-				   uint8_t vdev_id)
+static inline void lim_set_he_caps(struct mac_context *mac, struct pe_session *session,
+				   uint8_t *ie_start, uint32_t num_bytes,
+				   uint8_t band)
 {
 }
 
